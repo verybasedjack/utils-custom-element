85c213ae06cac5a849eeb9c3cf8a08ef081a1033
verybasedjack <>
2024-05-26 23:50:51 +0300

    feat: define helper

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,31 @@
-export {};
+import "./tests";
+
+function define(
+  name: string,
+  baseClass: typeof HTMLElement = HTMLElement,
+  initCallback?: (element: HTMLElement) => void
+) {
+  customElements.define(
+    name,
+    class extends baseClass {
+      constructor() {
+        super();
+      }
+
+      connectedCallback() {
+        this.initializeAttributes();
+        if (initCallback) {
+          initCallback(this);
+        }
+      }
+
+      initializeAttributes() {
+        Array.from(this.attributes).forEach((attr) => {
+          this[attr.name] = attr.value;
+        });
+      }
+    }
+  );
+}
+
+export { define };
---
+++ src/tests.ts
@@ -0,0 +1,24 @@
+import { define } from "./main";
+
+if (import.meta.env.DEV) {
+  // Error propagation for non-valid custom element tag name.
+  // define("abc");
+
+  // Anonymous custom element (default base, no connected callback).
+  define("x-a", void 0, console.log);
+
+  // Renders content to self.
+  define("x-b", void 0, (self) => {
+    self.innerHTML = "Content";
+  });
+
+  // Attributes redefined onto self & serialize.
+  define("x-c", void 0, (self) => {
+    console.log(JSON.stringify(self));
+  });
+
+  // Extend different base.
+  // define("x-d", HTMLButtonElement, (self) => {
+  //   // console.log(Object.getPrototypeOf(self));
+  // });
+}
